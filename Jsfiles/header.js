dc7bd6f713bd32276451e69eac7f151d5c725502
asudeh <a.asudeh@gmail.com>
2014-11-21 11:49:46 -0600

    V2

---
+++ Jsfiles/header.js
@@ -0,0 +1,24 @@
+// Written by Abolfazl Asudeh, 3/12/2013
+function header() {
+st =  '<table style="height:100%; width:100%">';
+st += '<tr>';
+st += '		<td width="200px" background="imgs/backgroud-topNL.gif">&nbsp;</td>';
+st += '		<td>';
+st += '		<table  height="100%" width="100%">';
+st += '			<tr>';
+st += '				<td>';
+st += '				<font color=#FFFFFF size="7">Abolfazl Asudeh</font>';
+st += '				<font size="2"  color=#FFFFFF><br>ab[DOT]asudeh[AT]mavs[DOT]uta[DOT]edu</font>';
+st += '				</td>';
+st += '				<td>';
+st += '				<font size="3"  color=#FFFFFF>Ph.D. Student,<a href="http://cse.uta.edu" target=_blank>Department of Computer Science</a>,<br><a href="http://www.uta.edu" target=_blank>University of Texas at Arlington</a></font><br>';
+st += '				<font size="2"  color=#FFFFFF>500, UTA Blvd, Engineering Research Building, Room 550<br>Arlington, TX 76019</font>';
+st += '				</td>';
+st += '			</tr>';
+st += '		</table>';
+st += '		</td>';
+st += '		<td width="175px;"><img src="imgs/1.JPG" class="niceBox"></td>';
+st += '	</tr>';
+st += '</table>';
+headerDiv.innerHTML = st;
+}
---
+++ Jsfiles/menu.js
@@ -0,0 +1,14 @@
+// Written by Abolfazl Asudeh, 3/12/2013
+
+function menu() {
+st = '<nav id="cssmenu"><ul>';
+st += '            <li><a href="index.htm">Home</a><br></li>';
+st+='            <li><a href="education.htm">Education</a><br></li>';
+st+='            <li><a href="research.htm">Research&Projects</a><br></li>';
+st+='            <li><a href="teaching.htm">Teaching Experiments</a><br></li>';
+st+='            <li><a href="industry.htm">Industry Experiments</a><br></li>';
+st+='            <li><a href="misc.htm">Misc.</a><br></li>';
+//st+='            <li><a target="_blank" href="cv_2.pdf">CV</a><br></li>';
+st+='</ul></nav>';
+menuBar.innerHTML = st;
+}
