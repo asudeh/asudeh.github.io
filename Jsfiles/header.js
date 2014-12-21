7dae152cd585fe4191455adde13e8a7073f13af7
asudeh <a.asudeh@gmail.com>
2014-12-21 14:34:55 -0600

    minor change

--- Jsfiles/header.js
+++ Jsfiles/header.js
@@ -11,7 +11,7 @@ st += '				<font color=#FFFFFF size="7">Abolfazl Asudeh</font>';
 st += '				<font size="2"  color=#FFFFFF><br>ab[DOT]asudeh[AT]mavs[DOT]uta[DOT]edu</font>';
 st += '				</td>';
 st += '				<td>';
-st += '				<font size="3"  color=#FFFFFF>Ph.D. Student,<a href="http://cse.uta.edu" target=_blank>Department of Computer Science</a>,<br><a href="http://www.uta.edu" target=_blank>University of Texas at Arlington</a></font><br>';
+st += '				<font size="3"  color=#FFFFFF>Ph.D. Student,&nbsp;<a href="http://cse.uta.edu" target=_blank>Department of Computer Science</a>,<br><a href="http://www.uta.edu" target=_blank>University of Texas at Arlington</a></font><br>';
 st += '				<font size="2"  color=#FFFFFF>500, UTA Blvd, Engineering Research Building, Room 550<br>Arlington, TX 76019</font>';
 st += '				</td>';
 st += '			</tr>';
