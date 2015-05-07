460bd226f11a383a6a8cbd9a3ba9df6168b20835
asudeh <a.asudeh@gmail.com>
2015-05-07 01:13:45 -0500

    --

--- Jsfiles/menu.js
+++ Jsfiles/menu.js
@@ -8,6 +8,7 @@ st+='            <li><a href="research.htm">Research&Projects</a><br></li>';
 st+='            <li><a href="teaching.htm">Teaching Experiments</a><br></li>';
 st+='            <li><a href="industry.htm">Industry Experiments</a><br></li>';
 st+='            <li><a href="misc.htm">Misc.</a><br></li>';
+st+='            <li><a href="codes.htm">Useful Stuff</a><br></li>';
 //st+='            <li><a target="_blank" href="cv_2.pdf">CV</a><br></li>';
 st+='</ul></nav>';
 menuBar.innerHTML = st;
