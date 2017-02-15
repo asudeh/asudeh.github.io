5080e49842972afe3e67af06f8c3dc76b2889fec
asudeh <a.asudeh@gmail.com>
2017-02-14 21:55:29 -0600

    --

--- Jsfiles/menu.js
+++ Jsfiles/menu.js
@@ -5,7 +5,7 @@ st = '<nav id="cssmenu"><ul>';
 st += '            <li><a href="index.htm">Home</a><br></li>';
 st+='            <li><a href="education.htm">Education</a><br></li>';
 st+='            <li><a href="research.htm">Research</a><br></li>';
-st+='            <li><a href="exp.htm">Experiments</a><br></li>';
+st+='            <li><a href="exp.htm">Experience</a><br></li>';
 st+='            <li><a href="misc.htm">Misc.</a><br></li>';
 st+='            <li><a href="codes.htm">Useful Stuff</a><br></li>';
 //st+='            <li><a target="_blank" href="cv_2.pdf">CV</a><br></li>';
