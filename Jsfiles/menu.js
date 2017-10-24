db9748361f2b435f73344d89b593cdad92f6c9fa
asudeh <a.asudeh@gmail.com>
2017-10-23 21:58:01 -0400

    --

--- Jsfiles/menu.js
+++ Jsfiles/menu.js
@@ -7,7 +7,7 @@ st+='            <li><a href="education.htm">Education</a><br></li>';
 st+='            <li><a href="research.htm">Research</a><br></li>';
 st+='            <li><a href="exp.htm">Experience</a><br></li>';
 st+='            <li><a href="misc.htm">Misc.</a><br></li>';
-st+='            <li><a href="codes.htm">Useful Stuff</a><br></li>';
+//st+='            <li><a href="codes.htm">Useful Stuff</a><br></li>';
 //st+='            <li><a target="_blank" href="cv_2.pdf">CV</a><br></li>';
 //st+='            <li><a href="farsi.htm">Farsi</a><br></li>';
 st+='</ul></nav>';
