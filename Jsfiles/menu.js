18098a1ae324db5e7e65d1b90472256165bc4e6c
asudeh <a.asudeh@gmail.com>
2018-03-03 01:14:09 -0500

    --

--- Jsfiles/menu.js
+++ Jsfiles/menu.js
@@ -3,8 +3,8 @@
 function menu() {
 st = '<nav id="cssmenu"><ul>';
 st += '            <li><a href="index.htm">Home</a><br></li>';
-st+='            <li><a href="education.htm">Education</a><br></li>';
 st+='            <li><a href="research.htm">Research</a><br></li>';
+st+='            <li><a href="education.htm">Academics</a><br></li>';
 st+='            <li><a href="exp.htm">Experience</a><br></li>';
 st+='            <li><a href="misc.htm">Misc.</a><br></li>';
 //st+='            <li><a href="codes.htm">Useful Stuff</a><br></li>';
