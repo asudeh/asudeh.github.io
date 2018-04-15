c1b19e863fdd8dcf5744d5e23bedf3d95cda894f
asudeh <a.asudeh@gmail.com>
2018-04-14 22:17:48 -0400

    --

--- Jsfiles/header.js
+++ Jsfiles/header.js
@@ -1,20 +1,20 @@
 // Written by Abolfazl Asudeh, 3/12/2013
 function header() {
 st =  '<table  height="100%" width="100%">';
-st+= '				<tr align="center"><td width="100px;">&nbsp;</td>';
+st+= '				<tr align="center"><td width="5px;">&nbsp;</td>';
 st+= ' 				<td align="left">';
-st+= ' 				<font size="6.5" color="#FFFFFF">Abolfazl Asudeh</font>';
+st+= ' 				<font size="6.5vm" color="#FFFFFF">Abolfazl Asudeh</font>';
 st+= ' 				</td>';
 st+= ' 				<td align="left">';
-st+= '				<font size="3" color="#FFFFFF">Research Fellow</font>';
+st+= '				<font size="3vm" color="#FFFFFF">Research Fellow</font>';
 st+= ' 				<br><a href="http://www.cse.umich.edu/" target=_blank>Department of Computer Science and Engineering</a>';
 st+= '				<br><a href="http://www.umich.edu" target=_blank>University of Michigan Ann Arbor</a>';
 st+= ' 				</td>';
 st+= '				<td align="left">';
-st+= '				<font size="3" color="#FFFFFF">asudeh[AT]umich[DOT]edu</font>';
+st+= '				<font size="3vm" color="#FFFFFF">asudeh[AT]umich[DOT]edu</font>';
 st+= '				<br>';
-st+= ' 				<font color="#FFFFFF" size="2">2260 Hayward St, Bob and Betty Beyster Building, Room 4945';
-st+= '				<br> Ann Arbor, MI 48109</font>';
+st+= ' 				<font color="#FFFFFF" size="2vm">2260 Hayward St, Bob and Betty Beyster Building';
+st+= '				<br> Room 4945, Ann Arbor, MI 48109</font>';
 st+= '				</td>';
 st+= ' 			</tr>';
 st+= '</table>';
