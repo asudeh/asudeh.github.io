da8cafa27f2c5658fc688b2c97d3be8d318ff070
asudeh <a.asudeh@gmail.com>
2018-04-14 21:52:12 -0400

    --

--- Jsfiles/header.js
+++ Jsfiles/header.js
@@ -1,24 +1,22 @@
 // Written by Abolfazl Asudeh, 3/12/2013
 function header() {
-st =  '<table style="height:100%; width:100%">';
-st += '<tr>';
-st += '		<td width="200px" <!--background="imgs/logo1.png"--> >&nbsp;</td>';
-st += '		<td>';
-st += '		<table  height="100%" width="100%">';
-st += '			<tr>';
-st += '				<td>';
-st += '				<font color=#FFFFFF size="7">Abolfazl Asudeh</font>';
-st += '				<font size="2"  color=#FFFFFF><br>asudeh[AT]umich[DOT]edu</font>';
-st += '				</td>';
-st += '				<td>';
-st += '				<font size="3"  color=#FFFFFF>Research Fellow,&nbsp;<a href="http://www.cse.umich.edu/" target=_blank>Department of Computer Science and Engineering</a>,<br><a href="http://www.umich.edu" target=_blank>University of Michigan Ann Arbor</a></font><br>';
-st += '				<font size="2"  color=#FFFFFF>2260 Hayward St, Bob and Betty Beyster Building, Room 4945<br>Ann Arbor, MI 48109</font>';
-st += '				</td>';
-st += '			</tr>';
-st += '		</table>';
-st += '		</td>';
-//st += '		<td width="175px;"><img height="163px" src="ppl/abol.jpg" class="niceBox"></td>';
-st += '	</tr>';
-st += '</table>';
+st =  '<table  height="100%" width="100%">';
+st+= '				<tr align="center"><td width="100px;">&nbsp;</td>';
+st+= ' 				<td align="left">';
+st+= ' 				<font size="6.5" color="#FFFFFF">Abolfazl Asudeh</font>';
+st+= ' 				</td>';
+st+= ' 				<td align="left">';
+st+= '				<font size="3" color="#FFFFFF">Research Fellow</font>';
+st+= ' 				<br><a href="http://www.cse.umich.edu/" target=_blank>Department of Computer Science and Engineering</a>';
+st+= '				<br><a href="http://www.umich.edu" target=_blank>University of Michigan Ann Arbor</a>';
+st+= ' 				</td>';
+st+= '				<td align="left">';
+st+= '				<font size="3" color="#FFFFFF">asudeh[AT]umich[DOT]edu</font>';
+st+= '				<br>';
+st+= ' 				<font color="#FFFFFF" size="2">2260 Hayward St, Bob and Betty Beyster Building, Room 4945';
+st+= '				<br> Ann Arbor, MI 48109</font>';
+st+= '				</td>';
+st+= ' 			</tr>';
+st+= '</table>';
 headerDiv.innerHTML = st;
 }
---
+++ Jsfiles/header_old.js
@@ -0,0 +1,24 @@
+// Written by Abolfazl Asudeh, 3/12/2013
+function header() {
+st =  '<table style="height:100%; width:100%">';
+st += '<tr>';
+st += '		<td width="200px" <!--background="imgs/logo1.png"--> >&nbsp;</td>';
+st += '		<td>';
+st += '		<table  height="100%" width="100%">';
+st += '			<tr>';
+st += '				<td>';
+st += '				<font color=#FFFFFF size="7">Abolfazl Asudeh</font>';
+st += '				<font size="2"  color=#FFFFFF><br>asudeh[AT]umich[DOT]edu</font>';
+st += '				</td>';
+st += '				<td>';
+st += '				<font size="3"  color=#FFFFFF>Research Fellow,&nbsp;<a href="http://www.cse.umich.edu/" target=_blank>Department of Computer Science and Engineering</a>,<br><a href="http://www.umich.edu" target=_blank>University of Michigan Ann Arbor</a></font><br>';
+st += '				<font size="2"  color=#FFFFFF>2260 Hayward St, Bob and Betty Beyster Building, Room 4945<br>Ann Arbor, MI 48109</font>';
+st += '				</td>';
+st += '			</tr>';
+st += '		</table>';
+st += '		</td>';
+//st += '		<td width="175px;"><img height="163px" src="ppl/abol.jpg" class="niceBox"></td>';
+st += '	</tr>';
+st += '</table>';
+headerDiv.innerHTML = st;
+}
