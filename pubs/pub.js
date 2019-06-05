f7b21efe048e74bd412176bc9521ad79f039b5ca
asudeh <a.asudeh@gmail.com>
2019-06-04 22:13:42 -0400

    --

--- pubs/pub.js
+++ pubs/pub.js
@@ -2,6 +2,84 @@
 // http://asudeh.github.io
 // June 2019
 
+var mypubs = [];
+
+function ReadBib(bibFile, divToFill)
+{ // Reads a bib file and passes it to the parser
+    var request = new XMLHttpRequest();
+    request.open('GET', bibFile, true);
+    request.send(null);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            return Parse(request.responseText, divToFill);
+        }
+    }
+}
+
+
+
+//--------------------- private functions, you don't need to use these ------------------------
+
+function Parse(st, divToFill)
+{ //Converts a bib file to its publication entries
+    mypubs = [];
+    entries = st.trim().split('@');
+    for(var l in entries)
+    {
+        var pub = {}
+        atoms = entries[l].split('\n');
+        for(i=1;i<atoms.length;i++)
+        {
+            line = atoms[i].trim();
+            if(line[0]=='%')
+            {
+                spl = line.substring(2, line.length).split(':');
+                //alert(spl[0].toLowerCase());
+                switch(spl[0].toLowerCase())
+                {
+                    case 'topic': pub['topic'] = spl[1].trim().split(";");
+                    break;
+                    case 'type': pub['type']= spl[1];
+                    break;
+                    case 'award': pub['award'] = spl[1].trim().split(";");
+                }
+                continue;
+            }
+        }
+        mypubs.push(pub);
+    }
+
+    /*
+    for(i=0;i<mypubs.length;i++)
+    {
+        var st = '';
+        var pub = mypubs[i];
+        for (var key in pub) st+=(key+': '+pub[key]+', ');
+        alert(st);
+    }
+    */
+    //return mypubs;
+    alert(Format());
+    document.getElementById(divToFill).innerHTML = Format();
+}
+
+function Format()
+{
+    var st='';
+    for(i=0;i<mypubs.length;i++)
+    {
+        st += '<p>';
+        var pub = mypubs[i];
+        for (var key in pub) st+=(key+': '+pub[key]+', ');
+        st+='</p>';
+    }
+    return st;
+}
+
+
+
+
+/*
 class Author{
     constructor(AuthorSt) {
         var res = AuthorSt.trim().split(",");
@@ -27,49 +105,4 @@ class Pub {
         this.awards = awardString.trim().split(";");
     }
 }
-
-
-function Parse(st)
-{ //Converts a bib file to its publication entries
-    alert('1');
-    var mypubs = [];
-    entries = st.trim().split('@');
-    for(var l in entries)
-    {
-        atoms = entries[l].split('\n');
-        var title=null, authors=null, venue=null, year=null, type=null, topics=null, page=page, vol=null, number=null, publisher=null, awards=null;
-        for(i=1;i<atoms.length;i++)
-        {
-            line = atoms[i].trim();
-            if(line[0]=='%')
-            {
-                spl = line.substring(2, line.length).split(':');
-                alert(spl[0].toLowerCase());
-                switch(spl[0].toLowerCase())
-                {
-                    case 'topic': topics = spl[1];
-                    break;
-                    case 'type': type = spl[1];
-                    alert(type);
-                    break;
-                    case 'award': awards = spl[1];
-                }
-                continue;
-            }
-        }
-        mypubs.push(new Pub(title=title, authors=authors,venue=venue,year=year,type=type,topicString=topics,page=page,vol=vol,number=number,publisher=publisher,awardString=awards));
-    }
-    return mypubs;
-}
-
-function ReadBib(bibFile)
-{ // Reads a bib file and passes it to the parser
-    var request = new XMLHttpRequest();
-    request.open('GET', bibFile, true);
-    request.send(null);
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
-            return Parse(request.responseText);
-        }
-    }
-}
+*/
