44be5f5ca09d875c7627cb4c52553ef5eaeba14f
asudeh <a.asudeh@gmail.com>
2019-06-04 21:00:33 -0400

    --

--- pubs/pub.js
+++ pubs/pub.js
@@ -13,7 +13,7 @@ class Author{
 }
 
 class Pub {
-    constructor(title, authors,venue,year,type=null,topicString,page=null,vol=null,number=null,publisher=null,awardString=null) {
+    constructor(title=null, authors=null,venue=null,year=null,type=null,topicString=null,page=null,vol=null,number=null,publisher=null,awardString=null) {
         this.title = title;
         this.authors = authors;
         this.venue = venue;
@@ -25,5 +25,51 @@ class Pub {
         this.number = number;
         this.publisher = publisher;
         this.awards = awardString.trim().split(";");
+    }
+}
+
+
+function Parse(st)
+{ //Converts a bib file to its publication entries
+    alert('1');
+    var mypubs = [];
+    entries = st.trim().split('@');
+    for(var l in entries)
+    {
+        atoms = entries[l].split('\n');
+        var title=null, authors=null, venue=null, year=null, type=null, topics=null, page=page, vol=null, number=null, publisher=null, awards=null;
+        for(i=1;i<atoms.length;i++)
+        {
+            line = atoms[i].trim();
+            if(line[0]=='%')
+            {
+                spl = line.substring(2, line.length).split(':');
+                alert(spl[0].toLowerCase());
+                switch(spl[0].toLowerCase())
+                {
+                    case 'topic': topics = spl[1];
+                    break;
+                    case 'type': type = spl[1];
+                    alert(type);
+                    break;
+                    case 'award': awards = spl[1];
+                }
+                continue;
+            }
+        }
+        mypubs.push(new Pub(title=title, authors=authors,venue=venue,year=year,type=type,topicString=topics,page=page,vol=vol,number=number,publisher=publisher,awardString=awards));
+    }
+    return mypubs;
+}
+
+function ReadBib(bibFile)
+{ // Reads a bib file and passes it to the parser
+    var request = new XMLHttpRequest();
+    request.open('GET', bibFile, true);
+    request.send(null);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            return Parse(request.responseText);
+        }
     }
 }
