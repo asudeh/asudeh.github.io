818333016016d6a978bdbfd675a5f04884de82db
asudeh <a.asudeh@gmail.com>
2020-01-12 00:24:11 -0600

    --

---
+++ js/myobjects.js
@@ -0,0 +1,91 @@
+// Abolfazl Asudeh, 3/2/2018
+function setup(ele,name)
+{
+	ele.innerHTML = '<a class="persontext" target="_blank" href="'+getURL(name)+'">'+getname(name)+'<br/>'+getaff(name)+'</a>';
+	//ele.innerHTML = '<span class="tooltiptext"><a class="text" target="_blank" href="'+getURL(name)+'">'+getname(name)+', '+getaff(name)+'</a></span>';
+	//ele.innerHTML = '<a class="text" target="_blank" href="'+getURL(name)+'">'+getname(name)+', '+getaff(name)+'</a>';
+	//ele.addEventListener("click", function(){clear(ele)});
+}
+
+function getURL(name){
+switch(name){
+	case "abol":
+		return "http://asudeh.github.io";
+	case "gautam":
+		return "http://ranger.uta.edu/~gdas/";
+	case "jag":
+		return "http://web.eecs.umich.edu/~jag/";
+	case "nan":
+		return "https://faculty.ist.psu.edu/nan/";
+	case "azade":
+		return "https://sites.google.com/site/azadenazi/";
+	case "julia":
+		return "https://engineering.nyu.edu/faculty/julia-stoyanovich";
+	case "gerome":
+		return "https://people.cs.umass.edu/~miklau/";
+	case "ke":
+		return "https://keyangatdrexel.github.io/";
+	case "bill":
+		return "https://faculty.washington.edu/billhowe/";
+	case "mark":
+		return "https://markjin1990.github.io/";
+	case "pranav":
+		return "https://www.linkedin.com/in/pranavmayuram";
+	default:
+		return "#";
+	}
+}
+
+function getaff(name){
+switch(name){
+	case "gautam":
+		return "University of Illinois at Chicago";
+	case "gautam":
+		return "University of Texas at Arlington";
+	case "nan":
+		return "Penn State University";
+	case "azade":
+		return "Google AI";
+	case "julia":
+		return "New York University";
+	case "gerome":
+		return "University of Massachusetts, Amherst";
+	case "bill":
+		return "University of Washington";
+	case "ke":
+		return "Drexel University";
+	default:
+		return "University of Michigan";
+	}
+}	
+
+function getname(name){
+switch(name){
+	case "abol":
+		return "Abolfazl Asudeh";
+	case "gautam":
+		return "Gautam Das";
+	case "jag":
+		return "H. V. Jagadish";
+	case "nan":
+		return "Nan Zhang";
+	case "azade":
+		return "Azade Nazi";
+	case "julia":
+		return "Julia Stoyanovich";
+	case "gerome":
+		return "Gerome Miklau";
+	case "bill":
+		return "Bill Howe";
+	case "ke":
+		return "Ke Yang";
+	case "mark":
+		return "Zhongjun (Mark) Jin";
+	case "chenkai":
+		return "Chenkai Sun";
+	case "yifan":
+		return "Yifan Guan";
+	case "pranav":
+		return "Pranav Mayuram";
+	}
+}		
---
+++ js/start.js
@@ -0,0 +1,41 @@
+function header(path='') {
+st = '<nav class="navbar navbar-expand-lg navbar-light bg-light"> ';
+st += '<div class="container collapse navbar-collapse" id="navbarSupportedContent"> ';
+st += '  <ul class="navbar-nav mr-auto"> ';
+st += '    <li class="nav-item active"> ';
+st += '      <a class="nav-link" href="'+path+'index.htm">Home <span class="sr-only">(current)</span></a> ';
+st += '    </li> ';
+st += '    <li class="nav-item active"> ';
+st += '      <a class="nav-link" href="'+path+'pub.htm">Publications <span class="sr-only">(current)</span></a> ';
+st += '    </li> ';
+st+= '     <li class="nav-item dropdown">';
+st+= '        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
+st+= '          Teaching';
+st+= '        </a>';
+st+= '        <div class="dropdown-menu" aria-labelledby="navbarDropdown">';
+st+= '          <a class="dropdown-item" href="'+path+'teaching/cs501spring20/index.html" target="_blank">CS501 Spring 2020</a>';
+st+= '          <hr>';
+st+= '          <a class="dropdown-item" href="'+path+'teaching/cs401fall19/index.html" target="_blank">CS401 Fall 2019</a>';
+st+= '        </div>';
+st += '  </ul> ';
+st += '</div> ';
+st += '</nav> ';
+/*
+st = '<table width="100%" id="header">';
+st+= '        <tr>';
+st+= '            <td width="10pt">&nbsp;</td>';
+st+= '            <td><img src="assets/mithra.png" height="52px"/></td>';
+st+= '            <td style="font-size:16pt; text-align:left; vertical-align: bottom; color:white; font-family:fantasy">Where Data Management meets Responsibility!</td>';
+st+= '            <td class="menue" align="right">';
+st+= '                <a href="index.htm">&#9751; Home</a><span width="10pt">&nbsp;&nbsp;&nbsp;</span>';
+st+= '                <a href="papers.htm">&#9776; Publication</a><span width="10pt">&nbsp;&nbsp;&nbsp;</span>';
+st+= '                <!--<a href="projects.htm">&#9741; Projects</a><span width="10pt">&nbsp;&nbsp;&nbsp;</span>-->';
+st+= '                <a href="tools.htm">&#9732; Tools</a><span width="10pt">&nbsp;&nbsp;&nbsp;</span>';
+st+= '                <a href="about.htm">&#9737; About</a>';
+st+= '            </td>';
+st+= '            <td width="10pt">&nbsp;</td>';
+st+= '        </tr>';
+st+= '</table>';
+*/
+headerDiv.innerHTML = st;
+}
