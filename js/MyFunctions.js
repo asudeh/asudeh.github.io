4c43df1bfd8e92655094808279a52d0884f83e09
asudeh <a.asudeh@gmail.com>
2020-03-02 02:10:32 -0600

    ..

--- js/MyFunctions.js
+++ js/MyFunctions.js
@@ -1,6 +1,6 @@
 function fillContent(File, divToFill){
     var request = new XMLHttpRequest();
-    request.open('GET', bibFile, true);
+    request.open('GET', File, true);
     //request.open('GET', 'https://asudeh.github.io/pubs/abol.bib', true);
     request.send(null);
     request.onreadystatechange = function () {
