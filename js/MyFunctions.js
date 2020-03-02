1cc2e507a6d20bec7a7f75b22640976a92e5fb57
asudeh <a.asudeh@gmail.com>
2020-03-02 14:44:05 -0600

    --

--- js/MyFunctions.js
+++ js/MyFunctions.js
@@ -1,10 +1,16 @@
-function fillContent(File, divToFill){
+function fillContent(File, divToFill=''){
+    if(divToFill=='') 
+    {
+        alert(1)
+        return this.innerHTML='test';
+    }
     var request = new XMLHttpRequest();
     request.open('GET', File, true);
     //request.open('GET', 'https://asudeh.github.io/pubs/abol.bib', true);
     request.send(null);
     request.onreadystatechange = function () {
         if (request.readyState === 4 && request.status === 200) {
+            if(divToFill=='') return this.innerHTML='test';//request.responseText;
             return document.getElementById(divToFill).innerHTML=request.responseText;
         }
     }
