8007f77fcff7db2eaa101f570b52c8a113d04e18
asudeh <a.asudeh@gmail.com>
2020-04-07 11:52:05 -0500

    --

--- indexlab/tmp/covid/js/mymap.js
+++ indexlab/tmp/covid/js/mymap.js
@@ -1,4 +1,3 @@
 var map = d3.geomap()
-        .geofile('d3-geomap/topojson/world/countries.json')
-        .draw(d3.select('#map'));
-
+    .geofile('d3-geomap/topojson/world/countries.json')
+    .draw(d3.select('#map'));
---
+++ indexlab/tmp/covid/readme.txt
@@ -0,0 +1,1 @@
+http://bl.ocks.org/hpfast/2fb8de57c356d8c45ce511189eec5d6a
