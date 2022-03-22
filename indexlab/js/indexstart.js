d38619f9164efff64d707362b244d6d9b8b1c306
Abolfazl Asudeh <a.asudeh@gmail.com>
2022-03-22 10:08:43 -0500

    --

--- indexlab/js/indexstart.js
+++ indexlab/js/indexstart.js
@@ -22,7 +22,7 @@ st+= '        </a>';
 st+= '        <div class="dropdown-menu" aria-labelledby="navbarDropdown">';
 st+= '          <a class="dropdown-item" href="'+path+'projects.htm">Summary</a>';
 st+= '          <hr>';
-st+= '          <a class="dropdown-item" href="'+path+'mithra.htm">Mithra: Responsible Data Science and Algorithmic Fairness</a>';
+st+= '          <a class="dropdown-item" href="'+path+'mithra.htm">Data Equity: Responsible Data Science and Algorithmic Fairness</a>';
 st+= '          <a class="dropdown-item" href="'+path+'FactChecking.htm">Computational Fact Checking</a>';
 st+= '          <a class="dropdown-item" href="'+path+'ranking.htm">Ranking & Representatives</a>';
 st+= '          <a class="dropdown-item" href="'+path+'orca.htm">Orca: Solving Problems at Scale</a>';
