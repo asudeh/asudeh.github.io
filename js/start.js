6ff1bdd41a7047c1cf844fa73197ebb350dc51a3
asudeh <a.asudeh@gmail.com>
2025-09-27 23:31:33 -0500

    --

--- js/start.js
+++ js/start.js
@@ -33,6 +33,7 @@ st+= '        </a>';
 st+= '        <div class="dropdown-menu" aria-labelledby="navbarDropdown">';
 st+= '          <a class="dropdown-item" href="'+path+'teaching/archive/cs516spring25/index.html" target="_blank">CS516 (Algorithmic Fairness) Spring 2025</a>';
 st+= '          <a class="dropdown-item" href="'+path+'teaching/archive/cs501fall21/index.html" target="_blank">CS501  (Algorithms II) Fall 2021</a>';
+st+= '          <a class="dropdown-item" href="'+path+'teaching/archive/cs480fall25/index.html" target="_blank">CS480  (Database Systems) Fall 2025</a>';
 st+= '          <hr>';
 st+= '          <a class="dropdown-item" href="'+path+'teaching/archive.html" target="_blank">Archive</a>';
 st+= '        </div>';
